ae9eab15c0f7574b2adc300eaeb4c8e6f6048e7e
MoXiR0422 <dilshodbekphone@gmail.com>
2023-03-24 17:15:10 +0500

    update

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const Dbconnect = require("./config/dbConnect")
+
+app.use(express.json)
+app.use(bodyParser.urlencoded({extended:true}))
+Dbconnect()
+
+const PORT = process.env.PORT || 8000
+
+
+app.listen(PORT,()=>{
+    console.log("server is running");
+})
